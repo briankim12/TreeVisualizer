207d969729d6f6e274955034c4a2b222b7c98f34
Brian Kim <briankim.ucsb@gmail.com>
2022-01-03 02:14:10 -0600

    Add error div

--- popup.js
+++ popup.js
@@ -1,7 +1,19 @@
 let inputTree = document.getElementById("inputTree");
 
 
+
+const init = function() {
+    document.getElementById("submit-build").addEventListener("click", build)
+}
+
+
+
+
 var output = document.getElementById("output")
-output.innerHTML = "PENIS";
+output.innerHTML = "Output Inner HTML";
+
+
+
+
 // Shows up On Inspect Element Developer
-console.log("hello")
+console.log("Hello From popup.js")
