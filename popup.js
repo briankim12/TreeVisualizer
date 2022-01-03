477b8d07be020cacf9bd72a3af11b8f5172e1b18
Brian Kim <briankim.ucsb@gmail.com>
2022-01-03 01:37:02 -0600

    Add popup.js to gather input from popup.html

---
+++ popup.js
@@ -0,0 +1,7 @@
+let inputTree = document.getElementById("inputTree");
+
+
+var output = document.getElementById("output")
+output.innerHTML = "PENIS";
+// Shows up On Inspect Element Developer
+console.log("hello")
