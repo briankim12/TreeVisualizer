3427820c78c8434a50ccbced0df7a532ded9412d
Brian Kim <briankim.ucsb@gmail.com>
2022-01-04 01:32:10 -0600

    add form control logic

--- popup.js
+++ popup.js
@@ -1,35 +1,58 @@
-const inputTreeString = document.getElementById("inputTreeString");
+const inputTree = document.getElementById("inputTree");
 const error = document.getElementById("error");
 const form = document.getElementById("form");
 const output = document.getElementById("output")
 // output.innerHTML = "Output Inner HTML";
 
 
+form.addEventListener("reset", (e) => {
+    // bring back default form-control
+    const formControl = inputTree.parentElement;
+    formControl.className = 'form-control';
+});
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    console.log(inputTreeString.value)
-    const inputTreeJSON = JSON.parse(inputTreeString.value)
 
 
-    console.log(inputTreeJSON)
+    validateInput();
 
+    // console.log(inputTree.value)
+    // const inputTreeJSON = JSON.parse(inputTree.value)
 
+    // console.log(inputTreeJSON)
 
-    error.innerText = "{typeof(inputTreeArray.value)}"
- 
-    
+    // error.innerText = "{typeof(inputTreeArray.value)}";
 
 });
 
-// const init = function() {
-//     document.getElementById("submit-build").addEventListener("click", build)
-// }
+function validateInput() {
+    const inputTreeString = inputTree.value.trim(); //trim() deletes whitespace
+
+    if (inputTreeString == '') {
+        setErrorFor(inputTree, "Input cannot be blank")
+    }
+
+    else {
+        setSuccessFor(inputTree)
+    }
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
 
 
-// const build = function(ev) {
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
 
-// }
+    // add error messagee inside small
+    small.innerText = message;
+    // add error class
+    formControl.className = 'form-control error';
+}
 
 
 
